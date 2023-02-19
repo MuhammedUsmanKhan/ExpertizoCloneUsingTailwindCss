fc8dd2d766a392a082f5c4a2632754974ced0de6
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-02-20 01:30:06 +0500

    WorkingFeatures

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   'logo': "url('/main/images/logo.png')",
-      //   // 'footer-texture': "url('/img/footer-texture.png')",
-      // }
+      backgroundImage: {
+        'work11': "url('/main/images/work11.png')",
+        'work12': "url('/main/images/work12.png')",
+        'work13': "url('/main/images/work13.png')",
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      },
       fontWeight: {
         extrabold: '800',
         'extra-bold': '650',
